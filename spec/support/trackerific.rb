1a09b901bb449f6f5c71009a91799e5874ca5368
Travis Haynes <travis.j.haynes@gmail.com>
2011-06-10 20:36:10 -0700

    Cleaned up the test app for the specs.

--- spec/support/trackerific.rb
+++ spec/support/trackerific.rb
@@ -3,7 +3,6 @@ require 'rails'
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
-require "rails/test_unit/railtie"
 
 module Trackerific
   class Application < Rails::Application
@@ -11,13 +10,6 @@ module Trackerific
   end
   
   Application.configure do
-    config.cache_classes = true
-    config.whiny_nils = true
-    config.consider_all_requests_local = true
-    config.action_controller.perform_caching = false
-    config.action_dispatch.show_exceptions = false
-    config.action_controller.allow_forgery_protection = false
-    config.action_mailer.delivery_method = :test
     config.active_support.deprecation = :stderr
   end
 end
