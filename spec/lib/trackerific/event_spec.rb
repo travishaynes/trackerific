810feec3f770de4d499e775ce4cd810a048a29a5
Travis Haynes <travis.j.haynes@gmail.com>
2011-06-14 15:37:45 -0700

    Added spec for Event.to_s

--- spec/lib/trackerific/event_spec.rb
+++ spec/lib/trackerific/event_spec.rb
@@ -3,18 +3,26 @@ require 'spec_helper'
 describe Trackerific::Event do
   before(:all) do
     @date = Time.now
-    @event = Trackerific::Event.new(Time.new, String.new, String.new)
+    @description = "description"
+    @location = "location"
+    @event = Trackerific::Event.new(@date, @description, @location)
   end
   
-  describe "date" do
+  describe :date do
     specify { @event.date.should be_a Time }
   end
   
-  describe "description" do
+  describe :description do
     specify { @event.description.should be_a String }
   end
   
-  describe "location" do
+  describe :location do
     specify { @event.location.should be_a String }
+  end
+  
+  describe :to_s do
+    it "should be in format mmm dd hh:mm am/pm.*" do
+      @event.to_s.should =~ /(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) (0[1-9]|[1-2][0-9]|3[01]) \d{2}:\d{2} (am|pm).*/
+    end
   end
 end
